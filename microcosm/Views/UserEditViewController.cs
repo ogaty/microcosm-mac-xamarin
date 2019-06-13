0f04ff3b85bd967a4f961674f282e678a9dec0ef
ogaty <trytodream1221@gmail.com>
2019-06-13 20:31:53 +0900

    userEdit 1

--- microcosm/Views/UserEditViewController.cs
+++ microcosm/Views/UserEditViewController.cs
@@ -6,7 +6,7 @@ using AppKit;
 
 namespace microcosm.Views
 {
-    public partial class UserEditViewController : AppKit.NSView
+    public partial class UserEditViewController : AppKit.NSViewController
     {
         #region Constructors
 
@@ -23,11 +23,96 @@ namespace microcosm.Views
             Initialize();
         }
 
+        // Call to load from the XIB/NIB file
+        public UserEditViewController() : base("UserEditView", NSBundle.MainBundle)
+        {
+            Initialize();
+        }
+
         // Shared initialization code
         void Initialize()
         {
         }
 
         #endregion
+
+        //strongly typed view accessor
+        public new UserEditView View
+        {
+            get
+            {
+                return (UserEditView)base.View;
+            }
+        }
+
+
+        partial void SubmitClicked(NSObject sender)
+        {
+            DateTime date;
+            if (birthDay.DateValue == null)
+            {
+                return;
+            }
+            try
+            {
+                NSDate nsd = birthDay.DateValue;
+                date = TimeZone.CurrentTimeZone.ToLocalTime(new DateTime(2001, 1, 1, 0, 0, 0));
+                date = date.AddSeconds(nsd.SecondsSinceReferenceDate);
+            }
+            catch (FormatException)
+            {
+                var alert = new NSAlert();
+                alert.MessageText = "エラー";
+                alert.InformativeText = "正しい日付を入れてください";
+                alert.RunModal();
+                return;
+            }
+            catch (InvalidCastException)
+            {
+                Console.WriteLine("ERROR: invalid cast.");
+                return;
+            }
+            /*
+            double lat = 35.685175, lng = 139.7528;
+            try
+            {
+                lat = double.Parse(UserLat.StringValue);
+                lng = double.Parse(UserLng.StringValue);
+            }
+            catch (InvalidCastException)
+            {
+                var alert = new NSAlert();
+                alert.MessageText = "エラー";
+                alert.InformativeText = "正しい緯度経度を入力してください";
+                alert.RunModal();
+                return;
+            }
+            */
+
+            if (fileName.StringValue == "")
+            {
+                var alert = new NSAlert();
+                alert.MessageText = "エラー";
+                alert.InformativeText = "ファイル名を入れてください";
+                alert.RunModal();
+                return;
+            }
+
+            if (userName.StringValue == "")
+            {
+                var alert = new NSAlert();
+                alert.MessageText = "エラー";
+                alert.InformativeText = "名前を入れてください";
+                alert.RunModal();
+                return;
+            }
+            UserDbViewController dbvc = this.PresentingViewController as UserDbViewController;
+            dbvc.ReSetDbTree();
+
+            // ファイル名が変わっていたら移動
+            // 元のファイル名を保持が必要
+
+            DismissViewController(this);
+        }
     }
 }
--- microcosm/Views/UserEditViewController.designer.cs
+++ microcosm/Views/UserEditViewController.designer.cs
@@ -1,9 +1,69 @@
-﻿namespace microcosm.Views
-{
+// WARNING
+//
+// This file has been generated automatically by Visual Studio to store outlets and
+// actions made in the UI designer. If it is removed, they will be lost.
+// Manual changes to this file may not be handled correctly.
+//
+using Foundation;
+using System.CodeDom.Compiler;
 
-    // Should subclass AppKit.NSViewController
-    [Foundation.Register("UserEditViewController")]
-    public partial class UserEditViewController
-    {
-    }
+namespace microcosm.Views
+{
+	[Register ("UserEditViewController")]
+	partial class UserEditViewController
+	{
+		[Outlet]
+		AppKit.NSDatePicker birthDay { get; set; }
+
+		[Outlet]
+		AppKit.NSTextField fileName { get; set; }
+
+		[Outlet]
+		AppKit.NSTextField furigana { get; set; }
+
+		[Outlet]
+		AppKit.NSTextField memo { get; set; }
+
+		[Outlet]
+		AppKit.NSTextField userName { get; set; }
+
+		[Outlet]
+		AppKit.NSTextField userPlace { get; set; }
+
+		[Action ("SubmitClicked:")]
+		partial void SubmitClicked (Foundation.NSObject sender);
+		
+		void ReleaseDesignerOutlets ()
+		{
+			if (fileName != null) {
+				fileName.Dispose ();
+				fileName = null;
+			}
+
+			if (userName != null) {
+				userName.Dispose ();
+				userName = null;
+			}
+
+			if (furigana != null) {
+				furigana.Dispose ();
+				furigana = null;
+			}
+
+			if (birthDay != null) {
+				birthDay.Dispose ();
+				birthDay = null;
+			}
+
+			if (userPlace != null) {
+				userPlace.Dispose ();
+				userPlace = null;
+			}
+
+			if (memo != null) {
+				memo.Dispose ();
+				memo = null;
+			}
+		}
+	}
 }
